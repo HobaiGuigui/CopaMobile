cf5f9eb0a5a8eb1ac0e806f318ce8fd98089e5cc
Hiobal Dine <hobai.sa@gmail.com>
2022-11-04 15:11:09 +0000

    Aula_1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_500Medium} from '@expo-google-fonts/roboto'
+
+import { Signin } from './src/screens/Signin';
+import { Loading } from './src/assets/components/Loading';
+
+import { THEME } from './src/assets/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts ({ Roboto_400Regular, Roboto_700Bold, Roboto_500Medium });
+    
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Signin /> : <Loading />}
+      </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "native-base"
+
+export function Signin() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+        
+          <Text color="#fff" fontSize={34} fontFamily="heading">
+            Signin
+            </Text>
+        </Center>
+    )
+}
